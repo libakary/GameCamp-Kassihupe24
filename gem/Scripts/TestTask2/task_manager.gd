fa058aacb205dbbad2e01bd64b6644a59966f278
Ozz <oskar.reineberg@gmail.com>
2024-10-12 14:08:12 +0300

    Fixed an error

--- gem/Scripts/TestTask2/task_manager.gd
+++ gem/Scripts/TestTask2/task_manager.gd
@@ -3,6 +3,6 @@ extends Node2D
 var buttonCount: int = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if (buttonCount >= 2):
 		get_parent().queue_free()
