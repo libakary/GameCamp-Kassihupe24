e33908a8f6d9f1c94f376da401b3e7b2e0e10263
Ozz <oskar.reineberg@gmail.com>
2024-10-12 19:57:48 +0300

    Changed Backgrounds

--- gem/Scripts/fade_to_night.gd
+++ gem/Scripts/fade_to_night.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 var nightSprite
+var fadeSpeed: float = 0.001
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +10,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	modulate.a -= 0.001
-	nightSprite.modulate.a += 0.001
+	modulate.a -= fadeSpeed
+	nightSprite.modulate.a += fadeSpeed
