0730ca835a2336a1a87bc08daec15f37d2564410
Karol <libakary@users.noreply.github.com>
2024-10-12 18:04:48 +0300

    Health bars kind of working

---
+++ gem/Scenes/sanity_bar.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var timer = get_node("TimerSanity")
+@onready var progressBar = get_node("TextureProgressBarSanity")
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	timer.wait_time = progressBar.value
+	timer.start()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#print(timer.time_left)
+	progressBar.value = timer.time_left
+
+#siia tuleks siis if timer on 30 then all kinds of shit h
+
+#iga node eraldi kutsuda ja kirjutada mis sellega juhtub
+
+func _on_timer_sanity_timeout():
+	print("Sanity 0")
