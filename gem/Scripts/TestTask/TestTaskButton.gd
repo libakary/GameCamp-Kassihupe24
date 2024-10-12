780c4086305fc50a4fa41a3985cca9e266cc1647
Ozz <oskar.reineberg@gmail.com>
2024-10-12 16:31:53 +0300

    Cannot move during Tasks and triggers are fixed

--- gem/Scripts/TestTask/TestTaskButton.gd
+++ gem/Scripts/TestTask/TestTaskButton.gd
@@ -4,4 +4,8 @@ extends Sprite2D
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
 		if get_rect().has_point(to_local(event.position)):
+			get_node("../../MainGameWindow/Mastermind").inTask = false
 			get_node("../").queue_free()
+
+func _process(_delta: float) -> void:
+	print(get_node("../../MainGameWindow/Mastermind"))
