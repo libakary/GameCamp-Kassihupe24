56eba65d8a0c16eb9746f6deac86fbc6923a87ee
MintsyMint <uku.jarvis@gmail.com>
2024-10-13 12:19:02 +0300

    Microwave game

--- gem/Scenes/hand.gd
+++ gem/Scenes/hand.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100 # Set a higher speed to follow the mouse faster
+const SPEED = 150 # Set a higher speed to follow the mouse faster
 
 func _physics_process(delta):
 	var direction = get_viewport().get_mouse_position() - self.position
--- gem/Scripts/micro_wave_game.gd
+++ gem/Scripts/micro_wave_game.gd
@@ -3,9 +3,11 @@ extends Node2D
 @onready var timer1 = get_node("Buttontimer1")
 var points = 0
 @onready var microbutton = $"Microwave button2/MicroButton"
+@onready var mouseFollowerRect = $TextureRect2 
 
 func _ready():
 	microbutton = false
+	process_mode = Node.PROCESS_MODE_PAUSABLE
 
 func _on_microwave_button_mouse_entered() -> void:
 	print ("mouse in")
@@ -13,7 +15,18 @@ func _on_microwave_button_mouse_entered() -> void:
 
 func _on_buttontimer_1_timeout() -> void:
 	points += 1
+	if points == 30:
+		points = points
 	print (points)
-	if points == 12:
-		print("now to start the thing..")
-		$"Microwave button2/MicroButton".enabled = true
+	
+
+
+func _on_termination_timer_timeout() -> void:
+	print("now to start the thing..")
+	
+	await get_tree().create_timer(3).timeout
+	get_tree().paused = true
+	var timespent : float
+	timespent = 10 * ((2 / (points/ 10)) +1)
+	print (timespent)
+	queue_free()
