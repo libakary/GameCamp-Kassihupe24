e439432b69453e9a242230eedb6d797756015f8e
Karu/Karol <53316391+libakary@users.noreply.github.com>
2024-10-13 16:31:58 +0300

    menu works!

--- gem/Scenes/quit_butt.gd
+++ gem/Scenes/quit_butt.gd
@@ -3,4 +3,4 @@ extends Sprite2D
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
 		if get_rect().has_point(to_local(event.position)):
-			get_tree().change_scene_to_file("res://Scenes/main_game_window.tscn")
+			get_tree().quit()
