a89d0a2ee10f3961d8e33aa705697194f2684497
MintsyMint <uku.jarvis@gmail.com>
2024-10-13 10:53:40 +0300

    Guitar minigame
    Sigma

--- gem/Scenes/guitar_minigame.gd
+++ gem/Scenes/guitar_minigame.gd
@@ -5,9 +5,10 @@ extends Node2D
 
 @onready var guitarkey_positions = $Spawnpoints
 
-var Points_total = 20
+var points_total = 20
 var points_lost = 0
 var points_gained = 0
+var points = points_total - points_lost
 
 func _on_guitar_timer_timeout() -> void:
 	var guitarkey_positions_array = guitarkey_positions.get_children()
@@ -16,7 +17,6 @@ func _on_guitar_timer_timeout() -> void:
 	var guitarkey = guitarkey_scene.instantiate()
 	guitarkey.global_position = random_guitarkey_position.global_position
 	add_child(guitarkey)
-
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_left"):
@@ -35,26 +35,31 @@ func _process(delta: float) -> void:
 		var guitarkill = guitarkey_kill.instantiate()
 		guitarkill.position = Vector2(314, 289)
 		add_child(guitarkill)
-	
 
 
-func _on_death_box_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	print("u missed")
+#func _on_death_box_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	#print("u missed")
+	#points_lost += 1
+	#points_gained += 1
+	#print (points_lost)
+	#if points_gained == 20:
+		#var points_left = Points_total - points_lost
+		#print (points_left)
+		#queue_free()
+#
+#func _on_guitar_key_0_key_killed() -> void:
+	#GainPoint()
+
+func _on_death_box_area_entered(area: Area2D) -> void:
+	print("U missed")
 	points_lost += 1
-	points_gained += 1
-	print (points_lost)
-	if points_gained == 20:
-		var points_left = Points_total - points_lost
-		print (points_left)
-		#terminate() peaks nagu olema see mis selle minigame'i kinni paneb
+	points_total - points_lost
+	#print(points_lost)
+	#print(points_total - points_lost)
 
-func _on_guitar_key_0_key_killed() -> void:
-	GainPoint()
+func _ready():
+	print(points_total - points_lost)
 
-func GainPoint():
-	points_gained += 1
-	print (points_gained)
-	if points_gained == 20:
-		var points_left = Points_total - points_lost
-		print (points_left)
-		#terminate()
+func _on_termination_timeout() -> void:
+	_ready()
+	queue_free()
