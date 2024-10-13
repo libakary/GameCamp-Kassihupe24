2abda44a26e3f8312f2091d98612e38146171761
Ozz <oskar.reineberg@gmail.com>
2024-10-13 10:48:22 +0300

    Added Progress Bar Interaction

--- gem/Scripts/TrashTask/TrashTrigger.gd
+++ gem/Scripts/TrashTask/TrashTrigger.gd
@@ -1,7 +1,39 @@
 extends Area2D
 
+var sanityBarIn
+var sanityTimerIn
+var sanityTimer
+var sanityMod = 20
+
+var healthBarIn
+var healthTimerIn
+var healthTimer
+var healthMod = 20
+
+func _ready() -> void:
+	healthBarIn = get_node("../../../MainGameWindow/Control/WorkBars/HealthBar/HBar")
+	sanityBarIn = get_node("../../../MainGameWindow/Control/WorkBars/SanityBar/SBar")
+	
+	healthTimerIn = get_node("../../../MainGameWindow/Control/WorkBars/HealthBar/TimerHealth") 
+	sanityTimerIn = get_node("../../../MainGameWindow/Control/WorkBars/SanityBar/TimerSanity") 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body == %Ball:
+		#print(get_node("../../../MainGameWindow/Control/WorkBars/HealthBar/HBar").value)
+		#get_node("../../../MainGameWindow/Control/WorkBars/WorkBar/WBar").value += 20
+		#print(get_node("../../../MainGameWindow/Control/WorkBars/HealthBar/HBar").value)
+		
+		#Add to health
+		healthTimer = healthTimerIn.time_left + healthMod
+		if (healthTimer > healthBarIn.max_value):
+			healthTimer = healthBarIn.max_value
+		healthTimerIn.start(healthTimer)
+		
+		#Add to sanity
+		sanityTimer = sanityTimerIn.time_left + sanityMod
+		if (sanityTimer > sanityBarIn.max_value):
+			sanityTimer = sanityBarIn.max_value
+		sanityTimerIn.start(sanityTimer)
+		
 		get_node("../../../MainGameWindow/Mastermind").inTask = false
 		get_node("../../").queue_free()
--- gem/Scripts/UI/sanity_bar.gd
+++ gem/Scripts/UI/sanity_bar.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var timer = get_node("TimerSanity")
-@onready var progressBar = get_node("TextureProgressBarSanity")
+@onready var progressBar = get_node("SBar")
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
